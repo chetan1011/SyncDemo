356e0c4b2804b61a42268f89ad737c85a5ff11a9
Chetan Patel <chetan.patel@indianic.com>
2017-12-19 11:48:11 +0530

    first commit

--- app/src/main/java/com/test/util/listener/DataReloader.java
+++
@@ -1,9 +0,0 @@
-package com.indianic.util.listener;
-
-/**
- * Provides the method to reload the data in view (from database, API etc.. )
- */
-public interface DataReloader {
-
-    void reloadData();
-}
